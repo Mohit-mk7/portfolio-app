6da9c804f97251925f87c9668b9287fc5197b620
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-20 21:28:20 +0530

    updated output

--- terraform-eks/outputs.tf
+++ terraform-eks/outputs.tf
@@ -6,7 +6,10 @@ output "eks_node_group_name" {
   value = module.eks.node_group_name
 }
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
-
-
-
+output "public_subnet_ids" {
+  value = [aws_subnet.public.id, aws_subnet.public_b.id]
+}
