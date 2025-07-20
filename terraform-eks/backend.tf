547eb2dde0bcc79dcb19639bdb83517b211e4404
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-20 17:50:36 +0530

    sim

--- terraform-eks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mohitbuc6789yy"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
-  }
-}
