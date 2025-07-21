3738909275aaee231ee7b50c40648f08924dab1b
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-21 11:58:46 +0530

    Update main.tf

--- bucket/main.tf
+++ bucket/main.tf
@@ -2,15 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-# Only create bucket if it doesn't already exist
 resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = true
-
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = [tags]
-  }
 }
 
 resource "aws_s3_bucket_versioning" "this" {
