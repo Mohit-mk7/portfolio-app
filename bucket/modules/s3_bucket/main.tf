7a94f5bdf9cb892f6d07015568a714896f4c0bcd
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-21 11:30:37 +0530

    Update main.tf

--- bucket/modules/s3_bucket/main.tf
+++ bucket/modules/s3_bucket/main.tf
@@ -1,10 +1,25 @@
+variable "bucket_name" {}
+
+data "aws_s3_bucket" "existing" {
+  bucket = var.bucket_name
+  lifecycle {
+    ignore_changes = [bucket]
+  }
+}
+
+locals {
+  bucket_exists = can(data.aws_s3_bucket.existing.id)
+}
+
 resource "aws_s3_bucket" "this" {
+  count  = local.bucket_exists ? 0 : 1
   bucket = var.bucket_name
   force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
+  count  = local.bucket_exists ? 0 : 1
+  bucket = aws_s3_bucket.this[0].id
 
   versioning_configuration {
     status = "Enabled"
