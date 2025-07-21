10000e6bc99199a53045e9e50f1159f6e2579481
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-22 00:47:37 +0530

    Update output.tf - eks

--- terraform-eks/modules/eks/output.tf
+++ terraform-eks/modules/eks/output.tf
@@ -5,3 +5,8 @@ output "cluster_name" {
 output "node_group_name" {
   value = aws_eks_node_group.private_nodes.node_group_name
 }
+
+
+output "public_node_group_name" {
+  value = aws_eks_node_group.public_nodes.node_group_name
+}
