a84b31c2c21be0d57f783edd6538fba10108b39a
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-22 09:31:52 +0530

    Update outputs.tf

--- terraform-eks/modules/vpc/outputs.tf
+++ terraform-eks/modules/vpc/outputs.tf
@@ -18,7 +18,9 @@ output "private_subnet_id" {
   value = aws_subnet.private.id
 }
 
-
+output "private_subnet_b_id" {
+  value = aws_subnet.private_b.id
+}
 
 output "alb_sg_id" {
   value = aws_security_group.alb.id
