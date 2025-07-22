548fb7b4d7671a4541e25d2e07bad5bbdd140e36
Mohit-mk7 <ghostmohit77@gmail.com>
2025-07-22 09:34:11 +0530

    Update outputs.tf

--- terraform-eks/modules/vpc/outputs.tf
+++ terraform-eks/modules/vpc/outputs.tf
@@ -7,6 +7,10 @@ output "public_subnet_id" {
   value = aws_subnet.public.id
 }
 
+output "public_subnet_b_id" {
+  value = aws_subnet.public_b.id
+}
+
 
 output "public_subnet_ids" {
   value = [aws_subnet.public.id, aws_subnet.public_b.id]
